146ed6ddeece4e3737667909cbc0010ce0dcdcd9
biruhA <biruhaemro.ba@gmail.com>
2023-04-13 17:15:05 +0300

    project setup finished

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -1,19 +1,13 @@
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text} from 'react-native';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {fonts} from '../theme/fonts';
 
 export function Navigation() {
   return (
     <NavigationContainer>
       <View>
-        <Text style={styles.bold}>Navigation</Text>
-        <Text style={{fontFamily: 'Poppins-Light'}}>Navigation</Text>
+        <Text>lively</Text>
       </View>
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  bold: fonts.body1,
-});
