8510a777038b1d39c2b73178fcccdedf32d89756
ANDUAMLAK TEMESGEN A <39644738+anditom@users.noreply.github.com>
2023-09-18 11:15:38 +0300

    Environments (#39)
    * ==ios===
    
    * configuration for android
    
    * git ignor env files
    
    * multiple environment ios configuration
    
    * finished env
    
    * ios run scripts added for multiple envs
    
    ---------
    
    Co-authored-by: Biruh <https://github.com/biruhA>

--- src/store/baseUrlConstant.ts
+++ src/store/baseUrlConstant.ts
@@ -3,8 +3,6 @@ import Config from 'react-native-config';
 
 export const baseUrl = Config.BASE_URL;
 
-console.log('🚀 ~ file: baseUrlConstant.ts:5 ~ baseUrl:', baseUrl);
-
 export const authUrl = fetchBaseQuery({
   baseUrl: `${baseUrl}/`,
 });
